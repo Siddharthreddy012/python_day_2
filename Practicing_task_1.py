56fb4603f5954ca037cc70ba9c5fd40259a715e7
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-06-27 18:30:25 +0530

    indexing

---
+++ Practicing_task_1.py
@@ -0,0 +1,6 @@
+d="HARRY POTTER"
+print("Given string :",d)
+print("showing space in given string :",d[-7])
+print("showing first occarance of t in given string :",d[9])
+print("showing second occarance of r in given string :",d[-9])
+print("showing o in the given string :",d[-5])
