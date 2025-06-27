4ff0b11cd9392234958353805763580ac25c3306
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-06-27 18:49:03 +0530

    indexing

---
+++ Practicing_task_3.py
@@ -0,0 +1,7 @@
+u="SONY BRAVIA"
+print("Given string :",u)
+print("showing space in given string :",u[4])
+print("showing B in given string :",u[-6])
+print("showing I in given string :",u[9])
+print("showing first letter in the given string :",u[0])
+print("showing last letter in the given string :",u[10])
