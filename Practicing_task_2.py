b941107b09c183fc50fe42c12fb7d942bce32db5
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-06-27 18:38:36 +0530

    indexing

---
+++ Practicing_task_2.py
@@ -0,0 +1,7 @@
+p="ALLU ARJUN"
+print("Given string :",p)
+print("showing second occarance of A in given string :",p[-5])
+print("showing second occarance of L in given string :",p[2])
+print("showing J in the given string :",p[-3])
+print("showing N in the given string :",p[9])
+
