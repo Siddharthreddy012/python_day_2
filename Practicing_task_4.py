b62f5822a3b7cd45f632145612c8837ddbc8f087
siddharthreddy012 <venkatsiddharthreddy027@gmail.com>
2025-06-27 18:58:42 +0530

    indexing

--- Practicing_task_4.py
+++ Practicing_task_4.py
@@ -1,5 +1,5 @@
-x=BINLADIN
+x='BINLADIN'
 print("Given string :",x)
-print("showing L & A in the given string :",x[-5],x[5])
+print("showing L & A in the given string :",x[-5],x[4])
 print("showing second occarance of I in the given string :",x[-2])
 print("showing first occarance of I in the given string :",x[1])
